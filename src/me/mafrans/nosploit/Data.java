bb21bcae0ec1d0c145bd11a40f7b050f97f1748f
Paul Reilly <paldiutheminecrafter@gmail.com>
2016-12-18 13:08:23 -0500

    Updated

--- src/me/mafrans/nosploit/Data.java
+++ src/me/mafrans/nosploit/Data.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2016 NoSploit
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package me.mafrans.nosploit;
 
 import java.util.ArrayList;
@@ -6,16 +21,14 @@ import java.util.List;
 import org.bukkit.entity.EntityType;
 import org.bukkit.inventory.ItemStack;
 
-public class Data
-{
+public class Data {
 
     public static List<ItemStack> books = new ArrayList();
     public static List<ItemStack> bookcd = new ArrayList();
     public static HashMap<Integer, Short> itemDataTable = new HashMap();
     public static List<EntityType> nonLivingEntities = new ArrayList();
 
-    public static void registerData()
-    {
+    public static void registerData() {
         // Non Living Entities
         {
             nonLivingEntities.add(EntityType.PLAYER);
