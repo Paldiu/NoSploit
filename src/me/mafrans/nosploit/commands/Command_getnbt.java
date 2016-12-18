3c027be3ada57b6a20c787ccd117f22d1c7efaf9
Paul Reilly <paldiutheminecrafter@gmail.com>
2016-12-18 13:09:07 -0500

    Updated

--- src/me/mafrans/nosploit/commands/Command_getnbt.java
+++ src/me/mafrans/nosploit/commands/Command_getnbt.java
@@ -1,7 +1,17 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2016 NoSploit
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package me.mafrans.nosploit.commands;
 
@@ -17,25 +27,20 @@ import org.bukkit.inventory.ItemStack;
  *
  * @author malmar03
  */
-public class Command_getnbt implements CommandExecutor
-{
+public class Command_getnbt implements CommandExecutor {
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
-    {
-        if (!(sender instanceof Player))
-        {
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!(sender instanceof Player)) {
             sender.sendMessage(ChatColor.RED + "This command may only be executed in game.");
             return true;
         }
-        if (args.length != 0)
-        {
+        if (args.length != 0) {
             return false;
         }
         Player psender = (Player) sender;
         if (psender.getInventory().getItemInMainHand() == null
-            || psender.getInventory().getItemInMainHand().getType() == Material.AIR)
-        {
+            || psender.getInventory().getItemInMainHand().getType() == Material.AIR) {
             sender.sendMessage(ChatColor.RED + "You need to hold an item to see its NBT data!");
             return true;
         }
